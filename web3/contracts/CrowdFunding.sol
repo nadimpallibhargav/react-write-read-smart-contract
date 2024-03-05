a50e344788acc80ccc64d47c5f1d68b86699a257
nadimpalli bhargav <bhargavraju98@gmail.com>
2024-03-05 15:01:20 +0000

    🚧 defined createCampaign function

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -9,7 +9,7 @@ contract CrowdFunding {
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -23,7 +23,30 @@ contract CrowdFunding {
     uint256 public numberOfCampaigns = 0;
 
     // function for creating a campaign
-    function createCampaign() {}
+        /* need '_' for defining parameters (ex: _title) */
+        /* need 'memory' for defining string parameters */
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        
+        Campaign storage campaign = campaigns[numberOfCampaigns]; // for updating the number of caompaigns
+
+        // check if the deadline is valid. if the deadline value is in past, show the invalid message        
+        require(campaign.deadline < block.timestamp, 'The deadline should be a date in the future.');
+
+        // continue if deadline is valid
+        // update the campaign with given params
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;        
+
+        numberOfCampaigns++; // increaments the total campaigns 
+
+        return numberOfCampaigns - 1; // returns the recently added campaign, that is campaigns[campaings.length - 1]
+
+    }
 
     // function for donating to campaign
     function dontateToCampaign() {}
