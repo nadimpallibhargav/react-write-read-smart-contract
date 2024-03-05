a9f8be00f28bc69270fcbe6349bdda8ae70f414e
nadimpalli bhargav <bhargavraju98@gmail.com>
2024-03-05 14:14:35 +0000

    🚧 define campaign object and no of campaigns

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+
+    // defining the campaign object
+    struct Campaign { 
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadlin;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    // mapping Campaign to access publicly
+    mapping(uint256 => Campaign) public campaigns;
+
+    // variable to store total no.of campaigns
+    uint256 public numberOfCampaigns = 0;
 }
